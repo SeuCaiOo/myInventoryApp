5cf9282aa43f440fbd4247c693f7b5e8d62c35b7
caiopimentel-97 <caiopimentel8@gmail.com>
2017-08-22 19:26:37 -0300

    Commit 1.12 - Bugado

--- app/src/main/java/com/example/caioalvesdasilva/inventoryapp/EditorActivity.java
+++ app/src/main/java/com/example/caioalvesdasilva/inventoryapp/EditorActivity.java
@@ -5,7 +5,7 @@ package com.example.caioalvesdasilva.inventoryapp;
  */
 
 import android.content.ContentValues;
-import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
@@ -20,7 +20,6 @@ import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.example.caioalvesdasilva.inventoryapp.data.CarContract;
-import com.example.caioalvesdasilva.inventoryapp.data.CarDbHelper;
 
 /**
  * Allows user to create a new pet or edit an existing one.
@@ -121,12 +120,6 @@ public class EditorActivity extends AppCompatActivity {
         int year = Integer.parseInt(yearString);
         int engine = (int) Float.parseFloat(engineString);
 
-        // Create database helper
-        CarDbHelper mDbHelper = new CarDbHelper(this);
-
-        // Gets the database in write mode
-        SQLiteDatabase db = mDbHelper.getWritableDatabase();
-
         // Create a ContentValues object where column names are the keys,
         // and pet attributes from the editor are the values.
         ContentValues values = new ContentValues();
@@ -137,16 +130,19 @@ public class EditorActivity extends AppCompatActivity {
         values.put(CarContract.CarEntry.COLUMN_CAR_FUEL, mFuel);
         values.put(CarContract.CarEntry.COLUMN_CAR_MILEAGE, mileage);
 
-        // Insert a new row for pet in the database, returning the ID of that new row.
-        long newRowId = db.insert(CarContract.CarEntry.TABLE_NAME, null, values);
+
+        // Insert a new pet into the provider, returning the content URI for the new pet.
+        Uri newUri = getContentResolver().insert(CarContract.CarEntry.CONTENT_URI, values);
 
         // Show a toast message depending on whether or not the insertion was successful
-        if (newRowId == -1) {
-            // If the row ID is -1, then there was an error with insertion.
-            Toast.makeText(this, "Error with saving pet", Toast.LENGTH_SHORT).show();
+        if (newUri == null) {
+            // If the new content URI is null, then there was an error with insertion.
+            Toast.makeText(this, getString(R.string.editor_insert_pet_failed),
+                    Toast.LENGTH_SHORT).show();
         } else {
-            // Otherwise, the insertion was successful and we can display a toast with the row ID.
-            Toast.makeText(this, "Pet saved with row id: " + newRowId, Toast.LENGTH_SHORT).show();
+            // Otherwise, the insertion was successful and we can display a toast.
+            Toast.makeText(this, getString(R.string.editor_insert_pet_successful),
+                    Toast.LENGTH_SHORT).show();
         }
     }
 
--- app/src/main/java/com/example/caioalvesdasilva/inventoryapp/data/CarProvider.java
+++ app/src/main/java/com/example/caioalvesdasilva/inventoryapp/data/CarProvider.java
@@ -11,11 +11,15 @@ import android.content.UriMatcher;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
+import android.util.Log;
 
 /**
  * {@link ContentProvider} for Pets app.
  */
 public class CarProvider extends ContentProvider {
+
+    /** Tag for the log messages */
+    public static final String LOG_TAG = CarProvider.class.getSimpleName();
 
     /** URI matcher code for the content URI for the pets table */
     private static final int CARS = 100;
@@ -109,8 +113,34 @@ public class CarProvider extends ContentProvider {
 
     @Override
     public Uri insert(Uri uri, ContentValues contentValues) {
-        return null;
+        final int match = sUriMatcher.match(uri);
+        switch (match) {
+            case CARS:
+                return insertPet(uri, contentValues);
+            default:
+                throw new IllegalArgumentException("Insertion is not supported for " + uri);
+        }
     }
+
+    /**
+     * Insert a pet into the database with the given content values. Return the new content URI
+     * for that specific row in the database.
+     */
+    private Uri insertPet(Uri uri, ContentValues values) {
+        // Get writeable database
+        SQLiteDatabase database = mDbHelper.getWritableDatabase();
+
+        // Insert the new pet with the given values
+        long id = database.insert(CarContract.CarEntry.TABLE_NAME, null, values);
+        // If the ID is -1, then the insertion failed. Log an error and return null.
+        if (id == -1) {
+            Log.e(LOG_TAG, "Failed to insert row for " + uri);
+            return null;
+        }
+        // Return the new URI with the ID (of the newly inserted row) appended at the end
+        return ContentUris.withAppendedId(uri, id);
+    }
+
 
     @Override
     public int delete(Uri uri, String s, String[] strings) {
