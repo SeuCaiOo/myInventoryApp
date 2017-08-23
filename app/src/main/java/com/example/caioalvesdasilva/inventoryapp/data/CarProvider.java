e5238d27368094dcb5cf6b40a2b566fb3ebbac4a
caiopimentel-97 <caiopimentel8@gmail.com>
2017-08-22 21:28:55 -0300

    1.14 - Bugado

--- app/src/main/java/com/example/caioalvesdasilva/inventoryapp/data/CarProvider.java
+++ app/src/main/java/com/example/caioalvesdasilva/inventoryapp/data/CarProvider.java
@@ -174,7 +174,78 @@ public class CarProvider extends ContentProvider {
     }
 
     @Override
-    public int update(Uri uri, ContentValues contentValues, String s, String[] strings) {
-        return 0;
+    public int update(Uri uri, ContentValues contentValues, String selection,
+                      String[] selectionArgs) {
+        final int match = sUriMatcher.match(uri);
+        switch (match) {
+            case CARS:
+                return updatePet(uri, contentValues, selection, selectionArgs);
+            case CAR_ID:
+                // For the PET_ID code, extract out the ID from the URI,
+                // so we know which row to update. Selection will be "_id=?" and selection
+                // arguments will be a String array containing the actual ID.
+                selection = CarContract.CarEntry._ID + "=?";
+                selectionArgs = new String[] { String.valueOf(ContentUris.parseId(uri)) };
+                return updatePet(uri, contentValues, selection, selectionArgs);
+            default:
+                throw new IllegalArgumentException("Update is not supported for " + uri);
+        }
+    }
+
+    /**
+     * Update pets in the database with the given content values. Apply the changes to the rows
+     * specified in the selection and selection arguments (which could be 0 or 1 or more pets).
+     * Return the number of rows that were successfully updated.
+     */
+    private int updatePet(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+        // If the {@link PetEntry#COLUMN_PET_NAME} key is present,
+        // check that the name value is not null.
+        if (values.containsKey(CarContract.CarEntry.COLUMN_CAR_BRAND)) {
+            String brand = values.getAsString(CarContract.CarEntry.COLUMN_CAR_BRAND);
+            if (brand == null) {
+                throw new IllegalArgumentException("Car requires a brand");
+            }
+        }
+
+        // If the {@link PetEntry#COLUMN_PET_NAME} key is present,
+        // check that the name value is not null.
+        if (values.containsKey(CarContract.CarEntry.COLUMN_CAR_MODEL)) {
+            String model = values.getAsString(CarContract.CarEntry.COLUMN_CAR_BRAND);
+            if (model == null) {
+                throw new IllegalArgumentException("Car requires a model");
+            }
+        }
+
+        // If the {@link PetEntry#COLUMN_PET_GENDER} key is present,
+        // check that the gender value is valid.
+        if (values.containsKey(CarContract.CarEntry.COLUMN_CAR_FUEL)) {
+            Integer fuel = values.getAsInteger(CarContract.CarEntry.COLUMN_CAR_FUEL);
+            if (fuel == null || !CarContract.CarEntry.isValidFuel(fuel)) {
+                throw new IllegalArgumentException("Car requires valid fuel");
+            }
+        }
+
+        // If the {@link PetEntry#COLUMN_PET_WEIGHT} key is present,
+        // check that the weight value is valid.
+        if (values.containsKey(CarContract.CarEntry.COLUMN_CAR_MILEAGE)) {
+            // Check that the weight is greater than or equal to 0 kg
+            Integer mileage = values.getAsInteger(CarContract.CarEntry.COLUMN_CAR_MILEAGE);
+            if (mileage != null && mileage < 0) {
+                throw new IllegalArgumentException("Car requires valid mileage");
+            }
+        }
+
+        // No need to check the breed, any value is valid (including null).
+
+        // If there are no values to update, then don't try to update the database
+        if (values.size() == 0) {
+            return 0;
+        }
+
+        // Otherwise, get writeable database to update the data
+        SQLiteDatabase database = mDbHelper.getWritableDatabase();
+
+        // Returns the number of database rows affected by the update statement
+        return database.update(CarContract.CarEntry.TABLE_NAME, values, selection, selectionArgs);
     }
 }
