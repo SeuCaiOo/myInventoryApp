a9946ae5923299b2cf252c08f1a853de3236a9e6
caiopimentel-97 <caiopimentel8@gmail.com>
2017-08-23 12:27:32 -0300

    1.4 - (1.19) - Switch to CursorLoader

--- app/src/main/java/com/example/caioalvesdasilva/inventoryapp/CatalogActivity.java
+++ app/src/main/java/com/example/caioalvesdasilva/inventoryapp/CatalogActivity.java
@@ -1,21 +1,30 @@
 package com.example.caioalvesdasilva.inventoryapp;
 
+import android.app.LoaderManager;
 import android.content.ContentValues;
+import android.content.CursorLoader;
 import android.content.Intent;
+import android.content.Loader;
 import android.database.Cursor;
 import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
-import android.support.design.widget.FloatingActionButton;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import com.example.caioalvesdasilva.inventoryapp.data.CarContract;
 
-public class CatalogActivity extends AppCompatActivity {
+public class CatalogActivity extends AppCompatActivity implements
+        LoaderManager.LoaderCallbacks<Cursor> {
+
+    /** Identifier for the pet data loader */
+    private static final int CAR_LOADER = 0;
+
+    /** Adapter for the ListView */
+    CarCursorAdapter mCursorAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,49 +47,15 @@ public class CatalogActivity extends AppCompatActivity {
         // Find and set empty view on the ListView, so that it only shows when the list has 0 items.
         View emptyView = findViewById(R.id.empty_view);
         petListView.setEmptyView(emptyView);
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        displayDatabaseInfo();
-    }
-
-    /**
-     * Temporary helper method to display information in the onscreen TextView about the state of
-     * the pets database.
-     */
-    private void displayDatabaseInfo() {
-
-        // Define a projection that specifies which columns from the database
-        // you will actually use after this query.
-        String[] projection = {
-                CarContract.CarEntry._ID,
-                CarContract.CarEntry.COLUMN_CAR_BRAND,
-                CarContract.CarEntry.COLUMN_CAR_MODEL,
-                CarContract.CarEntry.COLUMN_CAR_YEAR,
-                CarContract.CarEntry.COLUMN_CAR_ENGINE,
-                CarContract.CarEntry.COLUMN_CAR_FUEL,
-                CarContract.CarEntry.COLUMN_CAR_MILEAGE };
-
-        // Perform a query on the provider using the ContentResolver.
-        // Use the {@link PetEntry#CONTENT_URI} to access the pet data.
-        Cursor cursor = getContentResolver().query(
-                CarContract.CarEntry.CONTENT_URI,   // The content URI of the words table
-                projection,             // The columns to return for each row
-                null,                   // Selection criteria
-                null,                   // Selection criteria
-                null);                  // The sort order for the returned rows
-
-        // Find the ListView which will be populated with the pet data
-        ListView petListView = (ListView) findViewById(R.id.list);
 
         // Setup an Adapter to create a list item for each row of pet data in the Cursor.
-        CarCursorAdapter adapter = new CarCursorAdapter(this, cursor);
+        // There is no pet data yet (until the loader finishes) so pass in null for the Cursor.
+        mCursorAdapter = new CarCursorAdapter(this, null);
+        petListView.setAdapter(mCursorAdapter);
 
-        // Attach the adapter to the ListView.
-        petListView.setAdapter(adapter);
-    }
+        // Kick off the loader
+        getLoaderManager().initLoader(CAR_LOADER, null, this);
+        }
 
     /**
      * Helper method to insert hardcoded pet data into the database. For debugging purposes only.
@@ -119,7 +94,6 @@ public class CatalogActivity extends AppCompatActivity {
             case R.id.action_insert_dummy_data:
                 // Do nothing for now
                 insertPet();
-                displayDatabaseInfo();
                 return true;
             // Respond to a click on the "Delete all entries" menu option
             case R.id.action_delete_all_entries:
@@ -127,5 +101,34 @@ public class CatalogActivity extends AppCompatActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+        // Define a projection that specifies the columns from the table we care about.
+        String[] projection = {
+                CarContract.CarEntry._ID,
+                CarContract.CarEntry.COLUMN_CAR_BRAND,
+                CarContract.CarEntry.COLUMN_CAR_MODEL };
+
+        // This loader will execute the ContentProvider's query method on a background thread
+        return new CursorLoader(this,   // Parent activity context
+                CarContract.CarEntry.CONTENT_URI,   // Provider content URI to query
+                projection,             // Columns to include in the resulting Cursor
+                null,                   // No selection clause
+                null,                   // No selection arguments
+                null);                  // Default sort order
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        // Update {@link PetCursorAdapter} with this new cursor containing updated pet data
+        mCursorAdapter.swapCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        // Callback called when the data needs to be deleted
+        mCursorAdapter.swapCursor(null);
     }
 }
